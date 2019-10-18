5ced0fe545601df1651b87538055331d4f53bb65
System Administrator <root@Jorges-MacBook-Pro.local>
2019-10-18 15:42:21 -0400

    permitir ouvir usuários

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,20 +10,30 @@ export class HomePage {
   audiosCapturados: String[];
 
   constructor(private speechRecognition: SpeechRecognition) {
+
+  }
+
+  pedirPermissaoParaOuvir(){
     this.speechRecognition.hasPermission()
     .then((possuiPermissao: boolean) => {
       if(!possuiPermissao)
         this.speechRecognition.requestPermission();
+    }, (erro)=>{
+      console.log(erro);
     });
   }
 
   comecarAOuvir(){
     let options = {
-      language: 'pt-BR'
+      language: 'pt-BR',
+      showPartial: true,
+      match: 1000
+
     }
     this.speechRecognition.startListening(options)
-    .subscribe((audiosCapturados: string[]) => {
+    .subscribe((audiosCapturados: Array<string>) => {
       this.audiosCapturados = audiosCapturados;
+      console.log(audiosCapturados);
     });
   }
 
