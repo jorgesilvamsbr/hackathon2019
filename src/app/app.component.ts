5ee0ac3b4ff57fc035c011e81c5ab01768020c46
Gabriel Medina <gabrielsantos@digix.com.br>
2019-10-18 13:09:44 -0400

    Atualizando menu-ion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
